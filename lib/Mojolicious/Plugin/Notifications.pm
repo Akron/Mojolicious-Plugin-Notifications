4418e11ad4ffc193d8aa57eafd452320da6d6632
Akron <nils@diewald-online.de>
2018-05-30 11:51:28 +0200

    Introduced notify_html() function

--- lib/Mojolicious/Plugin/Notifications.pm
+++ lib/Mojolicious/Plugin/Notifications.pm
@@ -6,7 +6,7 @@ use Scalar::Util qw/blessed/;
 
 our $TYPE_RE = qr/^[-a-zA-Z_]+$/;
 
-our $VERSION = '1.01';
+our $VERSION = '1.02';
 
 # TODO:
 #   Maybe revert to tx-handler and use session instead of flash!
